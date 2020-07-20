0ce22a8c4dbd182ef5a8c583988385ef0aac361c
Manuel Hartwig <hartwig.manuel@web.de>
2020-07-20 21:12:14 +0200

    added timeline events
    politische ereignisse zeitleiste hinzugefügt

---
+++ Rupert Mayer - Konzept 1/Assets/05.scripts/TimelineRotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineRotate : MonoBehaviour
+{
+
+    public GameObject rotationObj; 
+    public bool RotOnOff = false; 
+    public float RotSpeed = 1;  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (RotOnOff) {
+            RotSpeed += 1;
+        }
+
+        rotationObj.transform.Rotate(0f,1f * RotSpeed, 0f);
+        
+       
+    }
+}
