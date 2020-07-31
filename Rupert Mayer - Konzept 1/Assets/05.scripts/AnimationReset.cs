1a7c7da40817ee58f99db68286eb2b2021140f8a
Manuel Hartwig <hartwig.manuel@web.de>
2020-08-01 01:23:59 +0200

    WW1 fertig, Naziszenen angefangen
    Animation reset in WW1 funktioniert, Erste Animationen und Assets für die Naziszenen integriert

---
+++ Rupert Mayer - Konzept 1/Assets/05.scripts/AnimationReset.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationReset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    Vector3 OrigPos;
+    private bool enabledObj = false;
+    private Animator Anim;
+    private int counter = 0;
+
+   
+   
+    void Awake()
+    {
+      
+      OrigPos = transform.localPosition;
+      Anim = GetComponent<Animator>();
+      counter += 1;       
+
+    }
+
+  void OnEnable() {
+
+    enabledObj = true;
+    
+
+  }
+  
+   void LateUpdate () {
+
+    if (enabledObj && counter > 1 ) {
+      
+      transform.localPosition = OrigPos;
+      enabledObj = false;
+
+      Anim.Play("MayerBackward");
+
+     
+    }
+    
+       
+   }
+
+  
+}
---
+++ Rupert Mayer - Konzept 1/Assets/05.scripts/RespawnScripts/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform spawnPos;
+    public GameObject spawnObj;
+
+    
+
+   
+
+    // Update is called once per frame
+    void OnEnable() {
+
+        Instantiate(spawnObj, spawnPos.position, spawnObj.transform.rotation, spawnPos );
+        
+    }
+}
